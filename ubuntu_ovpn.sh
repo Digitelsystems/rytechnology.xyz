3d1858d39a0cc8780da60e6d27d34ce7d6772ff5
Digitelsystems <103522787+Digitelsystems@users.noreply.github.com>
2022-06-03 03:14:04 -0700

    Update ubuntu_ovpn.sh

--- ubuntu_ovpn.sh
+++ ubuntu_ovpn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cp /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 #Database Details
-HOST='173.82.65.242';
-USER='rytechnology_rytechnology';
-PASS='dig291#0550013763';
-DBNAME='rytechnology_rytechnology';
+HOST='66.45.248.110';
+USER='digitelp_rytechnology';
+PASS='ME2HE2Di@';
+DBNAME='digitelp_rytechnology';
 
 install_require()
 {
